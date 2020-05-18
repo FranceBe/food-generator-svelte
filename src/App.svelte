47a81654a17ded304d2297f9547ff6eebdb5dbde
France Bérut <france.berut@gmail.com>
2020-05-18 12:10:23 +0200

    create selection buttons component and add style to main App

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,32 @@
 <script>
+	import { writable } from 'svelte/store';
 	import H1 from 'components/H1/H1.svelte';
+	import SelectionButton from 'components/SelectionButton/SelectionButton.svelte';
 	export let name;
+
+	const GET_RANDOM_RECIPES = 'Donnez moi des idées!'
+	const GET_RECIPES_FROM_FOOD_NAME = 'J\'ai une envie de...'
+
+	const selectedOption = writable('')
+	const selectOption = option => {
+		selectedOption.set(option)
+	}
 </script>
 
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+</style>
 <div>
 <H1>Hello {name}!</H1>
+	<SelectionButton
+		selectedOption={selectedOption}
+		onSelect={selectOption}
+		firstOption={GET_RANDOM_RECIPES}
+		secondOption={GET_RECIPES_FROM_FOOD_NAME}
+	/>
 </div>
---
+++ src/components/SelectionButton/SelectionButton.svelte
@@ -0,0 +1,60 @@
+<script>
+    export let firstOption;
+    export let secondOption;
+    export let onSelect;
+    export let selectedOption;
+
+    let active = ''
+    const firstOptionName = 'first-option'
+    const secondOptionName = 'second-option'
+    const handleSelect = option => {
+        onSelect(option);
+        active = option;
+    }
+</script>
+<style>
+    button {
+        height: 30px;
+        width: 300px;
+        font-size: 0.875rem;
+        font-weight: 700;
+        border: none;
+        background-color: #FFF;
+        color: #5F4C44;
+        text-decoration: none;
+        padding:0;
+    }
+    button:focus {
+        outline: none;
+    }
+    button.is-active {
+        background-color: #EBA789;
+        color: #FFF;
+        text-decoration: underline;
+    }
+    button:first-of-type {
+        border-radius: 10px 0 0 10px;
+        border: 1px solid #EBA789;
+    }
+    button:last-of-type {
+        border-radius: 0 10px 10px 0;
+        border-top: 1px solid #EBA789;
+        border-right: 1px solid #EBA789;
+        border-bottom: 1px solid #EBA789;
+    }
+</style>
+
+<div>
+    <button
+        class={active === firstOptionName ? 'is-active' : ''}
+        on:click={() => handleSelect(firstOptionName)}
+    >
+        {firstOption}
+    </button><button
+        class={active === secondOptionName ? 'is-active' : ''}
+        on:click={() => handleSelect(secondOptionName)}
+    >
+        {secondOption}
+    </button>
+</div>
+
