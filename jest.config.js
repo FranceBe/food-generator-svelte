1f0741c68e8d51871ad0688061c13f0d523b58e3
France Bérut <france.berut@gmail.com>
2020-05-18 16:56:56 +0200

    improve jest.config to allow coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,28 @@
+const baseDir = __dirname
+
 module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,svelte}',
+    '!src/**/*.test.{js,svelte}',
+    '!src/server/*.{js,svelte}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageDirectory: `${baseDir}/coverage`,
+  coverageReporters: ['text', 'html'],
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  rootDir: `${baseDir}`,
+  roots: [`${baseDir}/src`],
   transform: {
     "^.+\\.svelte$": "jest-transform-svelte",
     "^.+\\.js$": "babel-jest"
@@ -8,5 +32,6 @@ module.exports = {
   bail: false,
   verbose: true,
   transformIgnorePatterns: ["node_modules"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testRegex: '__tests__/.*\\.test\\.js$',
 };
